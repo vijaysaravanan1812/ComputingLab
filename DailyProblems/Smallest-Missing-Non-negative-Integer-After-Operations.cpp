6cac37e222d222ddc5f86faa38fa09bcb3f43c66
Vijaysaravanan <61357538+vijaysaravanan1812@users.noreply.github.com>
2025-10-16 11:25:56 +0530

    [2598] [Time Beats: 32.92%] [Memory Beats: 34.36%] - LeetPush

---
+++ DailyProblems/Smallest-Missing-Non-negative-Integer-After-Operations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findSmallestInteger(vector<int>& nums, int value) {
+        int num ;
+        unordered_map<int , int > mp;
+        for(int i = 0 ; i < nums.size() ; i++){
+            num  = nums[i];
+            if(num < 0){
+                num = (num % value) + value;
+            }
+            num = num % value;
+            mp[num]++;
+
+        }
+        int ans = 0 ; int req_val = 0;
+        while(1){ 
+            if(mp[req_val] > 0){
+                mp[req_val]--;
+                ans++;
+            }else{
+                break;
+            }
+            req_val = (req_val + 1) % value;
+        }
+        return ans;
+    }
+};
